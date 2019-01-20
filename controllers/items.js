4f0c18c36462d1e8d56cb5681eaad0b94f8d655a
AdrianMelGon <adrianmelgon@gmail.com>
2019-01-20 21:43:51 +0100

    done

--- controllers/items.js
+++ controllers/items.js
@@ -1,11 +1,51 @@
 const { Router } = require('express');
+const axios = require('axios');
+
+
+const restClashApi = axios.create(
+  { baseURL: 'http://www.clashapi.xyz/api/cards' }
+  )
+  
+  
+  function getClashInfo() {
+    return restClashApi.get()
+  }
+
+
+// let pokeArr = []
+
+// function getPokeApi(pokeId){
+//   return axios({
+//     url: `https://pokeapi.co/api/v2/pokemon/${pokeId}/`,
+//     dataType:'json',
+//   }).then(data => {
+//     pokeArr.push(data.data.id)
+//   }).catch( e  => console.log(e));
+// }
+
+// function promPokeInfo(){
+//   for(var i = 1; i <= 900; i++){
+//     getPokeApi(i)}
+//     return pokeArr
+// }
+
+
+
+
 
 module.exports = new Router()
   .get('/', (req, res) => {
-    // Code here
-    res.sendStatus(200);
+    let promClashInfo = new Promise(function (resolve, reject) {
+      let clashInfo = getClashInfo()
+      resolve(clashInfo)
+    })
+    promClashInfo.then(function (value) {
+      res.send(value.data.map((e) => {
+        return {
+          name: e.name,
+          id: e._id
+        }
+      })
+      )
+    })
   })
-  .get('/:id', (req, res) => {
-    // Code here
-    res.sendStatus(200);
-  });
